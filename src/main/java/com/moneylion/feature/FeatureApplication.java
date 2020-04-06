26f3ba49cdaaec8ff2294cc0bdcd01eefda3e955
Dmitry Sirotkin <sirotkin.dmitry@yahoo.com>
2020-04-06 21:34:57 +0800

    Implement feature caching
    Signed-off-by: Dmitry Sirotkin <sirotkin.dmitry@yahoo.com>

--- src/main/java/com/moneylion/feature/FeatureApplication.java
+++ src/main/java/com/moneylion/feature/FeatureApplication.java
@@ -3,8 +3,10 @@ package com.moneylion.feature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FeatureApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/moneylion/feature/FeatureRepository.java
+++ src/main/java/com/moneylion/feature/FeatureRepository.java
@@ -2,9 +2,12 @@ package com.moneylion.feature;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.cache.annotation.Cacheable;
 import java.util.Optional;
 
 @Repository
 public interface FeatureRepository extends CrudRepository<Feature, FeatureId> {
 
+  @Cacheable("feature")
+  Optional<Feature> findById(FeatureId id);
 }
